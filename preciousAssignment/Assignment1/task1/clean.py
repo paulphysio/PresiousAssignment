0d2f3814b9d42e4b3806353df51db5436eb77f86
Paul Obinna Madu <88602720+paulphysio@users.noreply.github.com>
2023-01-22 19:22:04 +0100

    Update clean.py

--- preciousAssignment/Assignment1/task1/clean.py
+++ preciousAssignment/Assignment1/task1/clean.py
@@ -1,40 +1,46 @@
 import pandas as pd
 import numpy as np
 import csv
+import os
 
 
 def clean(file):
     
-    main_list = []
-    df=pd.read_csv('Book1.csv', iterator=True, chunksize=10000, sep=";")
-    
-    for chunks in df:
-        chunks.to_csv('file.csv', index=False)
+    main = []
+    df=pd.read_csv(file, iterator=True, chunksize=1000, sep=";")
+
+    for chunk in df:
+        chunk.to_csv('file.csv', index=False)
         pf=pd.read_csv('file.csv')
-        chunked=pf.fillna('')
+        chunked=pf.fillna(' ')
+        
 
-        old_list=[]
-        new_list=[]
+        old_li=[]
+        new_li=[]
         for i in chunked.get("SiteID"):
-            if i=="" or type(i)==int:
-                old_list.append(i)
+            if i=="" or type(i)!=int:
+                old_li.append(i)
                 i=np.nan
-                new_list.append(i)
+                new_li.append(i)
             else:
-                old_list.append(i)
-                new_list.append(i)
+                old_li.append(i)
+                new_li.append(i)
 
-        chunked["SiteID"]=chunked["SiteID"].replace(old_list, new_list)
+        chunked["SiteID"]=chunked["SiteID"].replace(old_li, new_li)
         new_df=chunked.dropna()
 
 
-        main_list.append(new_df)
-    final_list=pd.concat(main_list)
-    final_list.to_csv("clean.csv", index=False)
+        main.append(new_df)
+    final_main=pd.concat(main)
+    final_main.to_csv("clean.csv", index=False)
+    os.remove('file.csv')
+    print("Do not open file yet, wait for code to finish running.....")
     with open('clean.csv', 'r') as file:
         data=file.read()
         data = data.replace(";", ",").replace("\"", "")
         with open("clean.csv", "w") as output:
             output.write(data)
+    return 'Done'
+
 
 clean("crop.csv")
